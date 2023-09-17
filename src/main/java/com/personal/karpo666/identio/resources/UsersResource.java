1f75e61c5ed64e5a9bbc7afdb8702c03f49d8305
Kare Lehtonen <lehtonenkare@gmail.com>
2023-09-17 23:38:10 +0300

    Started writing the main API for this project. - Created endpoints for various user-operations. - Added openapi-documentation. - Created tests for new endpoints.

---
+++ src/main/java/com/personal/karpo666/identio/resources/UsersResource.java
@@ -0,0 +1,139 @@
+package com.personal.karpo666.identio.resources;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.personal.karpo666.identio.clients.JsonPlaceholderClient;
+import com.personal.karpo666.identio.models.User;
+import com.personal.karpo666.identio.services.UsersService;
+import jakarta.enterprise.context.RequestScoped;
+import jakarta.inject.Inject;
+import jakarta.ws.rs.*;
+import jakarta.ws.rs.core.MediaType;
+import jakarta.ws.rs.core.Response;
+import lombok.extern.slf4j.Slf4j;
+import org.eclipse.microprofile.openapi.annotations.Operation;
+import org.eclipse.microprofile.openapi.annotations.media.Schema;
+import org.eclipse.microprofile.openapi.annotations.responses.APIResponse;
+import org.eclipse.microprofile.openapi.annotations.responses.APIResponseSchema;
+import org.eclipse.microprofile.openapi.annotations.tags.Tag;
+
+@RequestScoped
+@Tag(name = "Users")
+@Path("/api/v1")
+@Produces(MediaType.APPLICATION_JSON)
+@Consumes(MediaType.APPLICATION_JSON)
+@Slf4j
+public class UsersResource {
+
+    @Inject
+    UsersService usersService;
+
+    @GET
+    @Path("/users")
+    @Operation(
+        summary = "Fetch all users.",
+        description = "Get all users across JsonPlaceholder and mongodb."
+    )
+    @APIResponseSchema(value = User[].class, responseCode = "200", responseDescription = "Successfully fetched all users as an array.")
+    @APIResponse(responseCode = "404", description = "Users not found.")
+    @APIResponse(responseCode = "500", description = "Exception occurred when fetching users.")
+    public Response getAllUsers() {
+        log.debug("Incoming request for all users.");
+
+        // Attempt to get all users.
+        // Respond according to exceptions if one occurs.
+        try {
+            return Response.ok(usersService.getAllUsers()).build();
+
+        } catch (JsonPlaceholderClient.JsonPlaceHolderRestException e) {
+            log.error(e.getMessage());
+            return Response.status(e.getStatusCode()).build();
+
+        } catch (JsonProcessingException e) {
+            return Response.serverError().build();
+        }
+    }
+
+    @GET
+    @Path("/user")
+    @Operation(
+        summary = "Fetch user with specific id.",
+        description = "Search user with given id across JsonPlaceholder and mongodb."
+    )
+    @APIResponseSchema(value = User.class, responseCode = "200", responseDescription = "Successfully fetched user.")
+    @APIResponse(responseCode = "404", description = "User was not found.")
+    @APIResponse(responseCode = "400", description = "Bad request. Id might be empty or null.")
+    @APIResponse(responseCode = "500", description = "Exception occurred when fetching user.")
+    public Response getUser(@Schema(description = "User id. Cannot be null.", required = true) @QueryParam("id") String userId) {
+        log.debug("Incoming request for user with id: {}", userId);
+
+        // Validate userId.
+        if (userId.isEmpty()) {
+            return Response.status(400, "Id cannot be empty.").build();
+        }
+
+        // Attempt to get user with given id.
+        // Respond according to exceptions if one occurs.
+        try {
+            return Response.ok().entity(usersService.getUser(userId)).build();
+
+        } catch (JsonPlaceholderClient.JsonPlaceHolderRestException e) {
+            log.error(e.getMessage());
+            return Response.status(e.getStatusCode()).build();
+
+        } catch (JsonProcessingException e) {
+            return Response.serverError().build();
+        }
+    }
+
+    @POST
+    @Path("/user")
+    @Operation(
+        summary = "Create new user.",
+        description = "Create new user to mongodb."
+    )
+    @APIResponseSchema(value = User.class, responseCode = "201", responseDescription = "User saved to mongodb.")
+    @APIResponse(responseCode = "400", description = "Bad request. User id might not be empty or null.")
+    public Response createNewUser(@Schema(description = "New user info. Id must be left empty.", required = true) User user) {
+        log.debug("Incoming request to crate a new user.");
+
+        // Validate user.
+        if (user == null || user.getUserId() != null) {
+            return Response.status(400, "User id must be left empty.").build();
+        }
+
+        // Create new user to mongodb.
+        return Response.status(201).entity(usersService.createNewUser(user)).build();
+    }
+
+    @PATCH
+    @Path("/user")
+    @Operation(
+        summary = "Update existing user.",
+        description = "Update existing user."
+    )
+    @APIResponse(responseCode = "200", description = "User updated successfully.")
+    @APIResponse(responseCode = "404", description = "User was not found.")
+    @APIResponse(responseCode = "400", description = "Bad request. Id might be empty or null.")
+    @APIResponse(responseCode = "500", description = "Exception occurred when updating user.")
+    public Response updateExistingUser(@Schema(description = "Existing user info with changes made to it.", required = true) User user) {
+        log.debug("Incoming request to update user.");
+
+        // Validate user.
+        if (user == null || user.getUserId() == null || user.getUserId().isEmpty()) {
+            return Response.status(400, "User id must not be empty.").build();
+        }
+
+        // Attempt to update existing user.
+        try {
+            usersService.updateExistingUser(user);
+            return Response.status(200).build();
+
+        } catch (JsonPlaceholderClient.JsonPlaceHolderRestException e) {
+            log.error(e.getMessage());
+            return Response.status(e.getStatusCode()).build();
+
+        } catch (JsonProcessingException e) {
+            return Response.serverError().build();
+        }
+    }
+}
---
+++ src/test/java/com/personal/karpo666/identio/resources/UsersResourceTests.java
@@ -0,0 +1,285 @@
+package com.personal.karpo666.identio.resources;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.personal.karpo666.identio.FakeFactory;
+import com.personal.karpo666.identio.clients.JsonPlaceholderClient;
+import com.personal.karpo666.identio.models.User;
+import com.personal.karpo666.identio.services.UsersService;
+import io.quarkus.test.InjectMock;
+import io.quarkus.test.junit.QuarkusTest;
+import jakarta.inject.Inject;
+import jakarta.ws.rs.core.HttpHeaders;
+import jakarta.ws.rs.core.MediaType;
+import org.junit.jupiter.api.Test;
+
+import java.util.Collections;
+import java.util.List;
+
+import static io.restassured.RestAssured.given;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.*;
+
+@QuarkusTest
+class UsersResourceTests {
+
+    @InjectMock
+    UsersService usersService;
+
+    @Inject
+    ObjectMapper mapper;
+
+    @Test
+    void testGetUsers() throws Exception {
+        when(usersService.getAllUsers()).thenReturn(Collections.nCopies(10, FakeFactory.newUser()));
+        var resultString =
+            given()
+                .when()
+                .header(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON)
+                .header(HttpHeaders.ACCEPT, MediaType.APPLICATION_JSON)
+                .get("/api/v1/users")
+                .then()
+                .statusCode(200)
+                .extract().body().asString()
+        ;
+
+        assertNotNull(resultString);
+
+        List<User> users = mapper.readValue(resultString, new TypeReference<>() {});
+        assertNotNull(users);
+        assertEquals(10, users.size());
+
+        verify(usersService, times(1)).getAllUsers();
+    }
+
+    @Test
+    void testGetUsersNotFound() throws Exception {
+        when(usersService.getAllUsers())
+            .thenThrow(new JsonPlaceholderClient.JsonPlaceHolderRestException("No users found", 404))
+        ;
+
+        given()
+            .when()
+            .header(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON)
+            .header(HttpHeaders.ACCEPT, MediaType.APPLICATION_JSON)
+            .get("/api/v1/users")
+            .then()
+            .statusCode(404)
+        ;
+
+        verify(usersService, times(1)).getAllUsers();
+    }
+
+    @Test
+    void testGetUsersJsonProcessingException() throws Exception {
+        when(usersService.getAllUsers()).thenThrow(JsonProcessingException.class);
+        given()
+            .when()
+            .header(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON)
+            .header(HttpHeaders.ACCEPT, MediaType.APPLICATION_JSON)
+            .get("/api/v1/users")
+            .then()
+            .statusCode(500)
+        ;
+
+        verify(usersService, times(1)).getAllUsers();
+    }
+
+    @Test
+    void testGetUser() throws Exception {
+        final String userId = "TEST_USER_ID";
+        when(usersService.getUser(userId)).thenReturn(FakeFactory.newUser(userId));
+        var result =
+            given()
+                .when()
+                .header(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON)
+                .header(HttpHeaders.ACCEPT, MediaType.APPLICATION_JSON)
+                .queryParam("id", userId)
+                .get("/api/v1/user")
+                .then()
+                .statusCode(200)
+                .extract().body().as(User.class)
+        ;
+
+        assertNotNull(result);
+        assertEquals(userId, result.getUserId());
+
+        verify(usersService, times(1)).getUser(any(String.class));
+    }
+
+    @Test
+    void testGetUserEmptyUserId() {
+        given()
+            .when()
+            .header(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON)
+            .header(HttpHeaders.ACCEPT, MediaType.APPLICATION_JSON)
+            .queryParam("id", "")
+            .get("/api/v1/user")
+            .then()
+            .statusCode(400)
+        ;
+    }
+
+    @Test
+    void testGetUserNotFound() throws Exception {
+        when(usersService.getUser(any(String.class)))
+            .thenThrow(new JsonPlaceholderClient.JsonPlaceHolderRestException("User not found", 404))
+        ;
+
+        given()
+            .when()
+            .header(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON)
+            .header(HttpHeaders.ACCEPT, MediaType.APPLICATION_JSON)
+            .queryParam("id", "TEST")
+            .get("/api/v1/user")
+            .then()
+            .statusCode(404)
+        ;
+
+        verify(usersService, times(1)).getUser(any(String.class));
+    }
+
+    @Test
+    void testGetUserJsonProcessingException() throws Exception {
+        when(usersService.getUser(any(String.class))).thenThrow(JsonProcessingException.class)
+        ;
+
+        given()
+            .when()
+            .header(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON)
+            .header(HttpHeaders.ACCEPT, MediaType.APPLICATION_JSON)
+            .queryParam("id", "TEST")
+            .get("/api/v1/user")
+            .then()
+            .statusCode(500)
+        ;
+
+        verify(usersService, times(1)).getUser(any(String.class));
+    }
+
+    @Test
+    void testCreateUser() {
+        final String userId = "TEST_ID";
+        when(usersService.createNewUser(any(User.class))).thenReturn(FakeFactory.newUser(userId));
+
+        var result =
+            given()
+                .when()
+                .header(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON)
+                .header(HttpHeaders.ACCEPT, MediaType.APPLICATION_JSON)
+                .body(FakeFactory.newUser())
+                .post("/api/v1/user")
+                .then()
+                .statusCode(201)
+                .extract().body().as(User.class)
+        ;
+
+        assertNotNull(result);
+        assertEquals(userId, result.getUserId());
+
+        verify(usersService, times(1)).createNewUser(any(User.class));
+    }
+
+    @Test
+    void testCreateUserUserIdNotNull() {
+        given()
+            .when()
+            .header(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON)
+            .header(HttpHeaders.ACCEPT, MediaType.APPLICATION_JSON)
+            .body(FakeFactory.newUser("NOT_NULL"))
+            .post("/api/v1/user")
+            .then()
+            .statusCode(400)
+        ;
+    }
+
+    @Test
+    void testCreateUserUserNull() {
+        given()
+            .when()
+            .header(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON)
+            .header(HttpHeaders.ACCEPT, MediaType.APPLICATION_JSON)
+            .post("/api/v1/user")
+            .then()
+            .statusCode(400)
+        ;
+    }
+
+    @Test
+    void testUpdateUser() {
+        given()
+            .when()
+            .header(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON)
+            .header(HttpHeaders.ACCEPT, MediaType.APPLICATION_JSON)
+            .body(FakeFactory.newUser("TEST_ID"))
+            .patch("/api/v1/user")
+            .then()
+            .statusCode(200)
+        ;
+    }
+
+    @Test
+    void testUpdateUserNoUserId() {
+        given()
+            .when()
+            .header(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON)
+            .header(HttpHeaders.ACCEPT, MediaType.APPLICATION_JSON)
+            .body(FakeFactory.newUser())
+            .patch("/api/v1/user")
+            .then()
+            .statusCode(400)
+        ;
+    }
+
+    @Test
+    void testUpdateUserNullUser() {
+        given()
+            .when()
+            .header(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON)
+            .header(HttpHeaders.ACCEPT, MediaType.APPLICATION_JSON)
+            .body("")
+            .patch("/api/v1/user")
+            .then()
+            .statusCode(400)
+        ;
+    }
+
+    @Test
+    void testUpdateUserUserNotFound() throws Exception {
+        doThrow(new JsonPlaceholderClient.JsonPlaceHolderRestException("User not found.", 404))
+            .when(usersService).updateExistingUser(any(User.class))
+        ;
+
+        given()
+            .when()
+            .header(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON)
+            .header(HttpHeaders.ACCEPT, MediaType.APPLICATION_JSON)
+            .body(FakeFactory.newUser("TEST_ID"))
+            .patch("/api/v1/user")
+            .then()
+            .statusCode(404)
+        ;
+
+        verify(usersService, times(1)).updateExistingUser(any(User.class));
+    }
+
+    @Test
+    void testUpdateUserJsonProcessingException() throws Exception {
+        doThrow(JsonProcessingException.class).when(usersService).updateExistingUser(any(User.class));
+
+        given()
+            .when()
+            .header(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON)
+            .header(HttpHeaders.ACCEPT, MediaType.APPLICATION_JSON)
+            .body(FakeFactory.newUser("TEST_ID"))
+            .patch("/api/v1/user")
+            .then()
+            .statusCode(500)
+        ;
+
+        verify(usersService, times(1)).updateExistingUser(any(User.class));
+    }
+}
